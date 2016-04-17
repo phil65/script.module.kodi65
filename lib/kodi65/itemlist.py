a9b3a330828db43a6ba02d71e44c4f24639f4e63
phil65 <philipptemminghoff@gmx.de>
2016-04-17 22:25:29 +0200

    extend ItemList

--- lib/kodi65/itemlist.py
+++ lib/kodi65/itemlist.py
@@ -10,6 +10,8 @@ class ItemList(object):
         self.items = items
         self.name = name
         self.content_type = content_type
+        self.sorts = []
+        self.properties = []
 
     def __len__(self):
         return len(self.items)
@@ -17,7 +19,18 @@ class ItemList(object):
     def __iter__(self):
         return iter(self.items)
 
-    def add(self, item):
+    def __add__(self, other):
+        return ItemList(items=self.items + other.items,
+                        content_type=self.content_type,
+                        name=self.name,
+                        sorts=self.sorts,
+                        properties=self.properties)
+
+    def __iadd__(self, other):
+        self.items += other.items
+        return self
+
+    def append(self, item):
         self.items.append(item)
 
     def remove(self, index):
@@ -28,3 +41,19 @@ class ItemList(object):
 
     def set_content(self, content_type):
         self.content_type = content_type
+
+    def add_sorts(self, sorts):
+        self.sorts = sorts
+
+    def set_properties(self, properties):
+        self.properties = properties
+
+    def update_properties(self, properties):
+        self.properties.update({k: v for k, v in properties.iteritems() if v})
+
+    def set_property(self, key, value):
+        self.properties[key] = value
+
+    def get_property(self, key):
+        value = self.properties.get(key)
+        return value if value else ""
