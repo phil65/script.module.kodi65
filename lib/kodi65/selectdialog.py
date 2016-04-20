603ad59e37081268695a964256437983007a8f7f
phil65 <philipptemminghoff@gmx.de>
2016-04-21 00:26:25 +0200

    extend selectdialog functionality

--- lib/kodi65/selectdialog.py
+++ lib/kodi65/selectdialog.py
@@ -20,13 +20,18 @@ class SelectDialog(xbmcgui.WindowXMLDialog):
         xbmcgui.WindowXMLDialog.__init__(self)
         self.items = kwargs.get('listing')
         self.header = kwargs.get('header')
+        self.detailed = kwargs.get('detailed')
+        self.extrabutton = kwargs.get('extrabutton')
         self.listitems = [i.get_listitem() for i in self.items] if self.items else []
         self.index = -1
 
     def onInit(self):
         self.list = self.getControl(C_LIST_DETAIL)
-        self.getControl(C_LIST_SIMPLE).setVisible(False)
-        self.getControl(C_BUTTON_GET_MORE).setVisible(False)
+        self.getControl(C_LIST_DETAIL).setVisible(self.detailed)
+        self.getControl(C_LIST_SIMPLE).setVisible(not self.detailed)
+        self.getControl(C_BUTTON_GET_MORE).setVisible(bool(self.extrabutton))
+        if self.extrabutton:
+            self.getControl(C_BUTTON_GET_MORE).setLabel(self.extrabutton)
         self.getControl(C_LABEL_HEADER).setLabel(self.header)
         self.list.addItems(self.listitems)
         self.setFocus(self.list)
@@ -34,19 +39,23 @@ class SelectDialog(xbmcgui.WindowXMLDialog):
     def onClick(self, control_id):
         if control_id in [C_LIST_SIMPLE, C_LIST_DETAIL]:
             self.index = int(self.list.getSelectedPosition())
-            self.close()
+        elif control_id == C_BUTTON_GET_MORE:
+            self.index = -2
+        self.close()
 
     def onFocus(self, control_id):
         pass
 
 
-def open(listitems, header):
+def open(listitems, header, detailed=True, extrabutton=False):
     """
-    open selectdialog, return index
+    open selectdialog, return index (-1 for closing, -2 for extra button)
     """
     xbmc.executebuiltin("Dialog.Close(busydialog)")
     w = SelectDialog('DialogSelect.xml', addon.PATH,
                      listing=listitems,
-                     header=header)
+                     header=header,
+                     detailed=detailed,
+                     extrabutton=extrabutton)
     w.doModal()
     return w.index
