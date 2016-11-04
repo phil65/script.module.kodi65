01e6d16111fa150b66b708302a06408708260db5
phil65 <philipptemminghoff@gmx.de>
2016-11-04 16:01:29 +0100

    class-ify addon

--- lib/kodi65/__init__.py
+++ lib/kodi65/__init__.py
@@ -10,7 +10,9 @@ from busyhandler import busyhandler as busy
 from kodilogging import KodiLogHandler, config
 from dialogbaselist import DialogBaseList
 from localdb import LocalDB
+from kodiaddon import Addon
 from player import VideoPlayer
 
 local_db = LocalDB()
 player = VideoPlayer()
+addon = Addon()
