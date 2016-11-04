1800267bfdbb021282de41a3f5b8410720aacbb9
phil65 <philipptemminghoff@gmx.de>
2016-11-04 18:47:10 +0100

    init order

--- lib/kodi65/__init__.py
+++ lib/kodi65/__init__.py
@@ -3,6 +3,9 @@
 # Copyright (C) 2016 - Philipp Temminghoff <phil65@kodi.tv>
 # This program is Free Software see LICENSE file for details
 
+from kodiaddon import Addon
+addon = Addon()
+
 from listitem import ListItem, VideoItem, AudioItem
 from itemlist import ItemList
 from actionhandler import ActionHandler
@@ -10,9 +13,7 @@ from busyhandler import busyhandler as busy
 from kodilogging import KodiLogHandler, config
 from dialogbaselist import DialogBaseList
 from localdb import LocalDB
-from kodiaddon import Addon
 from player import VideoPlayer
 
 local_db = LocalDB()
 player = VideoPlayer()
-addon = Addon()
